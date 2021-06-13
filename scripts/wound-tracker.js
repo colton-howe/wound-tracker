48e679f42f1d6822ef69cb0d77aadc7e1dc69a2b
colton-howe <colton.howe@hotmail.ca>
2021-06-12 22:11:49 -0400

    v1.1 - Bug fixes & Wound Descriptions
    Fixed a bug where having 0 HP would not correctly update the actor HP tracker.  Added descriptions for what each wounds does in the button itself.

--- scripts/wound-tracker.js
+++ scripts/wound-tracker.js
@@ -12,6 +12,28 @@ const Wound = {
   CHEST: "chest",
   HEAD: "head"
 };
+const WoundDescription = {
+  leg: {
+    1: "-10ft Speed",
+    2: "Half Speed",
+    3: "0 Speed"
+  },
+  arm: {
+    1: "-2 on Attacks",
+    2: "Disadvantage on Attacks",
+    3: "No two handed attacks or spells"
+  },
+  chest: {
+    1: "-2 Str, Dex, Con Saves & Ability Checks",
+    2: "Disadvantage on Saves & Ability Checks",
+    3: "Gain 2 Exhasution Levels"
+  },
+  head: {
+    1: "-2 Wis, Int, Cha Saves & Ability Checks",
+    2: "Disadvantage on Concentration & Dex Checks",
+    3: "U ded"
+  }
+};
 
 //Wrapper around Actor._handleUpdate to grab previous HP, since its not supplied in the updateActor hook
 Hooks.on("init", () => {
@@ -19,7 +41,7 @@ Hooks.on("init", () => {
     const {request, result = [], userId} = args[0];
     result.map((data) => {
       const entity = Actor.collection.get(data._id, {strict: true})
-      if (entity && entity.data && entity.data.data && entity.data.data.attributes && entity.data.data.attributes.hp && entity.data.data.attributes.hp.value) {
+      if (entity && entity.data && entity.data.data && entity.data.data.attributes && entity.data.data.attributes.hp && entity.data.data.attributes.hp.value != null) {
         actorIdToHpMap[data._id] = entity.data.data.attributes.hp.value;
       } 
     });
@@ -35,7 +57,7 @@ Hooks.on("updateActor", (entity, entityChanges, options, userId) => {
 
   const changedActorData = entityChanges.data;
 
-  if (changedActorData.attributes && changedActorData.attributes.hp && changedActorData.attributes.hp.value) {
+  if (changedActorData.attributes && changedActorData.attributes.hp && changedActorData.attributes.hp.value != null) {
     const actorData = entity.data.data;
     const actorMaxHp = actorData.attributes.hp.max;
     const actorPreviousHp = actorIdToHpMap[entity.data._id];
@@ -43,17 +65,28 @@ Hooks.on("updateActor", (entity, entityChanges, options, userId) => {
     const newActorHp = changedActorData.attributes.hp.value;
     const hpDiff = actorPreviousHp - newActorHp;
     if (hpDiff > 0 && actorMaxHp) {
-      for (let i = 0; i < getNumberOfWounds(actorMaxHp, hpDiff); i++) {
-        const woundDialog = new Dialog({
-          title: "Wound Dialog",
-          content: "<p>Where would you like to apply your wound?</p>",
-          buttons: buildWoundButtonList(entity)
-        });
-        woundDialog.render(true);  
-      }
+      const numberOfWounds = getNumberOfWounds(actorMaxHp, hpDiff);
+      let i = 0;
+      showWoundDialogs(numberOfWounds, i, entity);
     }
   }
 });
+
+const showWoundDialogs = (totalWounds, currentWoundIndex, entity) => {
+  if (currentWoundIndex < totalWounds) {
+    const woundDialog = new Dialog({
+      title: "Wound Dialog",
+      content: "<p>Where would you like to apply your wound?</p>",
+      buttons: buildWoundButtonList(entity),
+      close: () => {
+        setTimeout(() => {
+          showWoundDialogs(totalWounds, ++currentWoundIndex, entity);
+        }, 100)
+      }
+    });
+    woundDialog.render(true);
+  }
+};
 
 const buildWoundButtonList = (actor) => {
   const legWounds = getMatchingWounds(actor, Wound.LEG);
@@ -63,34 +96,42 @@ const buildWoundButtonList = (actor) => {
 
   const availableWounds = {};
   if (legWounds.length < 3) {
+    const currentLegWound = legWounds.length + 1;
     availableWounds[Wound.LEG] = {
-      label: "Leg",
+      label: "Leg " + currentLegWound + getWoundDescription(Wound.LEG, currentLegWound),
       callback: () => applyWound(actor, Wound.LEG)
     };
   }
 
   if (armWounds.length < 3) {
+    const currentArmWound = armWounds.length + 1;
     availableWounds[Wound.ARM] = {
-      label: "Arm",
+      label: "Arm " + currentArmWound + getWoundDescription(Wound.ARM, currentArmWound),
       callback: () => applyWound(actor, Wound.ARM)
     };
   }
 
   if (chestWounds.length < 3) {
+    const currentChestWound = chestWounds.length + 1;
     availableWounds[Wound.CHEST] = {
-      label: "Chest",
+      label: "Chest " + currentChestWound + getWoundDescription(Wound.CHEST, currentChestWound),
       callback: () => applyWound(actor, Wound.CHEST)
     };
   }
 
   if (headWounds.length < 3) {
+    const currentHeadWounds = headWounds.length + 1;
     availableWounds[Wound.HEAD] = {
-      label: "Head",
+      label: "Head " + currentHeadWounds + getWoundDescription(Wound.HEAD, currentHeadWounds),
       callback: () => applyWound(actor, Wound.HEAD)
     };
   }
   return availableWounds;
 };
+
+const getWoundDescription = (woundName, woundCount) => {
+  return " (" + WoundDescription[woundName][woundCount] + ")";
+}
 
 const applyWound = (actor, woundType) => {
   const woundTypeID = STATUS_PREFIX + woundType + "-";
